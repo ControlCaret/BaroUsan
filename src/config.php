39f10a168d636a70a0b2fcff282e0a64b68bfc7d
Heejae Kwak <controlcaret@gmail.com>
2021-05-15 22:22:51 +0900

    Add MySQL config

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+    $config = array
+    (
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => 'password',
+        'dbname' => 'BaroUsan'
+    );
+    return $config;
+?>
